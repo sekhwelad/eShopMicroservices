7013a0460d43d2201f244630be3b75cee099fd3c
sekhwelad <25sekhwelad@gmail.com>
2023-01-31 21:28:33 +0200

    Created Product Repository

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByCategory(string name);
+        Task CreateProduct (Product product);   
+        Task<bool> UpdateProduct(Product product);  
+        Task<bool> DeleteProduct(string id);
+    }
+}
