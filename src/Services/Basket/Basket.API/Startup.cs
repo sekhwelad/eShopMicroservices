711f9d215d8c4f279303d1c3a9426587a1085fd6
sekhwelad <25sekhwelad@gmail.com>
2023-02-02 01:13:50 +0200

    Added Redis Cache

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -22,9 +22,12 @@ namespace Basket.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetValue<string>("CasheSettings:ConnectionString");
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -33,7 +36,6 @@ namespace Basket.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
