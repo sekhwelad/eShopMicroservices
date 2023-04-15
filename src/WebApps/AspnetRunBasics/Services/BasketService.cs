15f6f2d69c100f9b548346d8618c9107bc95bdaa
sekhwelad <25sekhwelad@gmail.com>
2023-04-15 09:00:57 +0200

    API Service Implementations

--- src/WebApps/AspnetRunBasics/Services/BasketService.cs
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -1,23 +1,44 @@
-﻿using AspnetRunBasics.Models;
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class BasketService : IBasketService
     {
-        public Task CheckoutBasket(BasketCheckoutModel model)
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
         {
-            throw new System.NotImplementedException();
+            _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
-        public Task<BasketModel> GetBasket(string username)
+        public async Task CheckoutBasket(BasketCheckoutModel model)
         {
-            throw new System.NotImplementedException();
+            var response = await _client.PostAsJson($"/Basket/Checkout", model);
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling the API.");
+            }
         }
 
-        public Task<BasketModel> UpdateBasket(BasketModel model)
+        public async Task<BasketModel> GetBasket(string username)
         {
-            throw new System.NotImplementedException();
+            var response = await _client.GetAsync($"/Basket/{username}");
+            return await response.ReadContentAs<BasketModel>(); 
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            var response = await _client.PostAsJson($"/Basket", model);
+            if(response.IsSuccessStatusCode)
+                return await response.ReadContentAs<BasketModel>();
+            else
+            {
+                throw new Exception("Something went wrong when calling the API.");
+            }
         }
     }
 }
--- src/WebApps/AspnetRunBasics/Services/OrderService.cs
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -1,14 +1,25 @@
-﻿using AspnetRunBasics.Models;
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUsername(string username)
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
         {
-            throw new System.NotImplementedException();
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUsername(string username)
+        {
+            var response = await _client.GetAsync($"/Order/{username}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
 }
